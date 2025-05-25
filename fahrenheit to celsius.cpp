f24c63c54a11c71ce3c323791e838064faf59b77
Israt Arefin <nisratafrin826@gmail.com>
2025-05-25 23:19:57 +0600

    Add files via upload

---
+++ fahrenheit to celsius.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    double farn , cels;
+    cout<< "Enter Fahrenheit = ";
+    cin>> farn;
+
+    cels = (farn-32)/1.8;
+
+    cout<< "celsius = " << cels;
+
+    getch();
+}
